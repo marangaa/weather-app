6a4e5055f5c8cbe0e18633a70b153955fd247ec3
Maranga <rchdmaranga@gmail.com>
2023-05-11 15:27:45 +0300

    Add working weather app code
    This commit adds the necessary changes to the `whether.js` file to make the weather app work. The changes include:
    
    - Fixing the API URL in the `getWeather` function to use the `location` parameter
    - Changing the variable names in the `updateUI` function to match the weather data object properties
    - Updating the query selector IDs in the `locationForm` and `updateUI` functions to match the HTML document
    - Fixing a typo in the `index.html` file where the `script` tag was misspelled
    
    The app now successfully retrieves weather data based on user input and updates the UI to display the temperature, weather description, and location.

--- whether.js
+++ whether.js
@@ -15,9 +15,42 @@ locationForm.addEventListener('submit', async (e) => {
 
 async function getWeather(location) {
   const apiKey = '92f4af4d809cd7f72786b822dd641409';
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`;
+  const geocodingUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${apiKey}`;
+  const geocodingResponse = await fetch(geocodingUrl);
+  const geocodingData = await geocodingResponse.json();
+  const { lat, lon } = geocodingData[0];
 
-  const response = await fetch(apiUrl);
-  return await response.json();
+  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+  const weatherResponse = await fetch(weatherUrl);
+  return await weatherResponse.json();
 }
 
+
+
+function updateUI(weather) {
+  const temperature = weather.main.temp; // Define temperature variable using the weather data
+  const weatherDescription = weather.weather[0].description;
+  const location = weather.name;
+
+  // Create a div to hold the weather data
+  const weatherDiv = document.createElement('div');
+  weatherDiv.classList.add('weather-data');
+
+  // Create and append elements for temperature, weather description, and location
+  const tempElement = document.createElement('p');
+  tempElement.textContent = `Temperature: ${temperature}°C`;
+  weatherDiv.appendChild(tempElement);
+
+  const descElement = document.createElement('p');
+  descElement.textContent = `Weather: ${weatherDescription}`;
+  weatherDiv.appendChild(descElement);
+
+  const locElement = document.createElement('p');
+  locElement.textContent = `Location: ${location}`;
+  weatherDiv.appendChild(locElement);
+
+  // Append the weather div to the weather-info div in the HTML
+  const weatherInfo = document.querySelector('#weather-info');
+  weatherInfo.innerHTML = ''; // Clear any previous weather data
+  weatherInfo.appendChild(weatherDiv);
+}
